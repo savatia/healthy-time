7e8e0275634206ae9b9e12fa56f1075b6ad4dded
sharon malio <kanini1996@gmail>
2016-07-25 16:32:24 +0300

    activities page

--- healthy-time/application/views/includes/header.php
+++ healthy-time/application/views/includes/header.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-    <link rel="stylesheet" href="http://localhost/healthy-time/healthy-time/application/assets/vendor/css/material.min.css" />
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendor/css/material.min.css" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
     <title> <?= $title ?> </title>
