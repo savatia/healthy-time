428245c9df8010e16fe3ef89a60e5824599d217c
Brian Savatia Keyonzo <briankeyonzo@yahoo.com>
2016-07-28 14:49:19 +0300

    Url testing

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,5 +1,5 @@
 <script src="<?php echo base_url() ?>application/assets/vendor/js/material.min.js" /> </script>
-<script src="<?php echo base_url() ?>application/assets/vendor/js/jquery.min.js" /> </script>
+<script src="/application/assets/vendor/js/jquery.min.js" /> </script>
 
 <?php $this->load->view('includes/messages/error.php'); ?>
 </body>
